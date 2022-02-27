dc34bea76627e0f83f4fc2180d8a599b0ddb2518
zoders <sanyafil61802@gmail.com>
2022-02-28 01:47:33 +0300

    Update app.lua

--- src/app.lua
+++ src/app.lua
@@ -25,7 +25,7 @@ box.once('schema',
 		)
 	end
 )
-
+server = httpd.new('0.0.0.0', get_port("PORT", 5000))
 local function info(req)
 	local resp = req:render{json = {
         api = {
@@ -168,7 +168,7 @@ local function get_port(env_port, default)
     return port
 end
 
-local server = httpd.new('0.0.0.0', get_port("PORT", 5000))
+
 server:route({ path = '/', method = 'GET' }, info)
 server:route({ path = '/kv', method = 'POST' }, create)
 server:route({ path = '/kv/:key', method = 'DELETE' }, delete)
