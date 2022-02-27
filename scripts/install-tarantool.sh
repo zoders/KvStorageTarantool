054173a2d62ecfdc3030ddc9a134e9ad069ec1dc
zoders <sanyafil61802@gmail.com>
2022-02-27 22:27:55 +0300

    )

--- scripts/install-tarantool.sh
+++ scripts/install-tarantool.sh
@@ -18,3 +18,12 @@ apt-get update
 apt-get -y install tarantool
 
 tarantoolctl rocks install http
+git clone https://github.com/tarantool/nginx_upstream_module.git nginx_upstream_module
+cd nginx_upstream_module
+git submodule update --init --recursive
+git clone https://github.com/nginx/nginx.git nginx
+
+# Ubuntu
+apt-get install libpcre++0 gcc unzip libpcre3-dev zlib1g-dev libssl-dev libxslt-dev
+
+make build-all
